0f35f40a2d07512261733593e0143b84e170b0f1
Surender Adavalli <surenderreddy23@gmail.com>
2026-07-21 13:46:19 +1000

    lesson2 param and query validation

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Post, Query } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
@@ -7,7 +7,11 @@ export class PropertyController {
         return 'This action returns all properties';
     }
     @Get(':id')
-    findOne(@Param('id') id: string): string {
+    findOne(@Param('id', ParseIntPipe) id, @Query('sort', ParseBoolPipe) sort): string {
+        console.log('id', id);
+        console.log('typeof id', typeof id);
+        console.log('sort', sort);
+        console.log('typeof sort', typeof sort);
         return `This action returns property with ID: ${id}`;
     }
     @Post('create')
